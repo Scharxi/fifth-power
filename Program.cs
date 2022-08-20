e467ea50212adc83416ac3dbe26bd189dedbbe13
BufferOverflow <scharxidev@outlook.de>
2022-08-20 16:22:49 +0200

    Initial Commit 🚀

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Function that returns the first 100 odd numbers
+IEnumerable<int> GetFirstHundredOddNumbers()
+{
+    return Enumerable.Range(1, 200).ToList().FindAll(a => a % 2 == 0).Take(100).ToList();
+}
+
+// Returns the sum of the first hundred odd numbers
+long GetSumOfFirstHundredOddNumbers()
+{
+    return GetFirstHundredOddNumbers().Sum();
+}
+
+var fifthRoot = Math.Pow(Convert.ToDouble(GetSumOfFirstHundredOddNumbers()), 5);
+Console.WriteLine(fifthRoot);
